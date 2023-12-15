f1d81bb8bd0563b07069e9d2a403f061646d9f72
2.D Krška Adam <Adam.Krska@student.sps-cl.cz>
2023-12-15 08:45:13 +0100

    zacatek implementace quick Sortu

--- krska algoritmus/quickSort.js
+++ krska algoritmus/quickSort.js
@@ -1,21 +1,18 @@
 class QuickSortAnim extends sortAnim {
     
     async sortAsc(){
-        for (var i = 1; i < this.values.length; i++) {
-            let j = i -1
-            while(j>= 0 && this.values[j] > this.values[j + 1]){
-                this.setCompareColor(this.columns[j]);
-                this.setCompareColor(this.columns[j+1]);
-                await this.sleep(100);
-                this.setDefaultColor(this.columns[j]);
-                this.setDefaultColor(this.columns[j+1]);
-                this.swapValues(j,j + 1)
-                j--
-            }
+        this.sortRecursive(this.values,0,this.values.length - 1)
+    }
+
+    async sortRecursive(){
+        if(low<high){
+            let pi = this
         }
-        for (let index = 0; index < i; index++) {
-            this.setSortedColor(this.columns[index]);
-            
-        }
+    }
+         
+    
+    for (let index = 0; index < i; index++) {
+        this.setSortedColor(this.columns[index]);
+        
     }
 }
